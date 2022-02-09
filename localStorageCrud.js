206620b563c284796ce317a447099b72a5b24c3b
takshdaffodil <taksh@daffodilsw.com>
2022-02-09 18:17:12 +0530

    important fixes

--- localStorageCrud.js
+++ localStorageCrud.js
@@ -1,31 +1,32 @@
-const form = document.getElementById('formData');
+const form = document.getElementById("formData");
 let update = false;
-let updateId = '';
+let updateId = "";
+let creationDate;
 
-const firstName = form.elements['firstName'];
-const lastName = form.elements['lastName'];
-const companyName = form.elements['companyName'];
-const address = form.elements['address'];
-const email = form.elements['email'];
-const phone = form.elements['phone'];
-const additionalInfo = form.elements['addInfo'];
+const firstName = form.elements["firstName"];
+const lastName = form.elements["lastName"];
+const companyName = form.elements["companyName"];
+const address = form.elements["address"];
+const email = form.elements["email"];
+const phone = form.elements["phone"];
+const additionalInfo = form.elements["addInfo"];
 
-const FIRST_NAME_REQUIRED = 'Please enter firstname';
-const LAST_NAME_REQUIRED = 'Please enter lastname';
-const COMPANY_NAME_REQUIRED = 'Please enter company name';
-const ADDRESS_REQUIRED = 'Please enter address';
-const EMAIL_REQUIRED = 'Please enter email';
-const PHONE_REQUIRED = 'Please enter phone';
-const INFO_REQUIRED = 'Please enter info';
+const FIRST_NAME_REQUIRED = "Please enter firstname";
+const LAST_NAME_REQUIRED = "Please enter lastname";
+const COMPANY_NAME_REQUIRED = "Please enter company name";
+const ADDRESS_REQUIRED = "Please enter address";
+const EMAIL_REQUIRED = "Please enter email";
+const PHONE_REQUIRED = "Please enter phone";
+const INFO_REQUIRED = "Please enter info";
 
 function uniqueId() {
   return Math.random().toString(16).slice(2);
 }
 
 function showMessage(input, message, type) {
-  const msg = input.parentNode.querySelector('p');
+  const msg = input.parentNode.querySelector("p");
   msg.innerText = message;
-  msg.setAttribute('class', 'text-danger');
+  msg.setAttribute("class", "text-danger");
   // type
   //   ? input.setAttribute("class", "is-valid form-control")
   //   : input.setAttribute("class", "is-invalid form-control");
@@ -37,83 +38,87 @@ function showError(input, message) {
 }
 
 function showSuccess(input) {
-  return showMessage(input, '', true);
+  return showMessage(input, "", true);
 }
 
 function hasValue(input, message) {
-  if (input.value.trim() === '') {
+  if (input.value.trim() === "") {
     return showError(input, message);
   }
   return showSuccess(input);
 }
 
 function saveToLocalStorage(dataToSave) {
-  let users = JSON.parse(localStorage.getItem('users') || '[]');
+  let users = JSON.parse(localStorage.getItem("users") || "[]");
   users.push(dataToSave);
-  localStorage.setItem('users', JSON.stringify(users));
+  localStorage.setItem("users", JSON.stringify(users));
 }
 
 function updateInLocalStorage(dataToUpdate) {
-  let users = JSON.parse(localStorage.getItem('users'));
+  let users = JSON.parse(localStorage.getItem("users"));
+  let index = users.findIndex((user) => user.id === dataToUpdate.id);
   let usersToSaveAgain = users.filter((user) => user.id !== dataToUpdate.id);
-  usersToSaveAgain.push(dataToUpdate);
-  localStorage.setItem('users', JSON.stringify(usersToSaveAgain));
+  usersToSaveAgain.splice(index, 0, dataToUpdate);
+  localStorage.setItem("users", JSON.stringify(usersToSaveAgain));
 }
 
 function showLocalStorageData() {
-  if (localStorage.getItem('users') !== null) {
-    let users = JSON.parse(localStorage.getItem('users'));
-    let userCards = document.getElementById('userCards');
-    userCards.innerHTML = '';
+  if (localStorage.getItem("users") !== null) {
+    let users = JSON.parse(localStorage.getItem("users"));
+    let userCards = document.getElementById("userCards");
+    userCards.innerHTML = "";
     users.map((user) => {
-      let div = document.createElement('div');
-      div.setAttribute('class', 'card m-3');
-      div.setAttribute('id', `${user.id}`);
-      div.style.width = '18rem';
+      let div = document.createElement("div");
+      div.setAttribute("class", "card m-3");
+      div.setAttribute("id", `${user.id}`);
+      div.style.width = "20rem";
 
-      let ul = document.createElement('ul');
-      ul.setAttribute('class', 'list-group list-group-flush');
+      let ul = document.createElement("ul");
+      ul.setAttribute("class", "list-group list-group-flush");
 
-      let liOne = document.createElement('li');
-      liOne.setAttribute('class', 'list-group-item');
+      let liOne = document.createElement("li");
+      liOne.setAttribute("class", "list-group-item");
       liOne.textContent = `Name: ${user.firstName} ${user.lastName}`;
 
-      let liTwo = document.createElement('li');
-      liTwo.setAttribute('class', 'list-group-item');
+      let liTwo = document.createElement("li");
+      liTwo.setAttribute("class", "list-group-item");
       liTwo.textContent = `Address: ${user.address}`;
 
-      let liThree = document.createElement('li');
-      liThree.setAttribute('class', 'list-group-item');
+      let liThree = document.createElement("li");
+      liThree.setAttribute("class", "list-group-item");
       liThree.textContent = `CompanyName: ${user.companyName}`;
 
-      let liFour = document.createElement('li');
-      liFour.setAttribute('class', 'list-group-item');
+      let liFour = document.createElement("li");
+      liFour.setAttribute("class", "list-group-item");
       liFour.textContent = `Email: ${user.email}`;
 
-      let liFive = document.createElement('li');
-      liFive.setAttribute('class', 'list-group-item');
+      let liFive = document.createElement("li");
+      liFive.setAttribute("class", "list-group-item");
       liFive.textContent = `Phone: ${user.phone}`;
 
-      let updateButton = document.createElement('button');
-      updateButton.setAttribute('class', 'btn btn-info m-1');
-      updateButton.textContent = 'Update';
+      let liSix = document.createElement("li");
+      liSix.setAttribute("class", "list-group-item");
+      liSix.textContent = `created at ${user.date.toLocaleString("en-GB")}`;
+
+      let updateButton = document.createElement("button");
+      updateButton.setAttribute("class", "btn btn-info m-1");
+      updateButton.textContent = "Update";
       updateButton.onclick = () => updateUser(user.id);
 
-      let deleteButton = document.createElement('button');
-      deleteButton.setAttribute('class', 'btn btn-danger m-1');
-      deleteButton.textContent = 'Delete';
-      // deleteButton.onclick = () => deleteUser(user.id);
-      document.getElementById('deleteUserButton').onclick = () =>
+      let deleteButton = document.createElement("button");
+      deleteButton.setAttribute("class", "btn btn-danger m-1");
+      deleteButton.textContent = "Delete";
+      document.getElementById("deleteUserButton").onclick = () =>
         deleteUser(user.id);
-      console.log(user.id);
-      deleteButton.setAttribute('data-bs-toggle', 'modal');
-      deleteButton.setAttribute('data-bs-target', '#staticBackdrop');
+      deleteButton.setAttribute("data-bs-toggle", "modal");
+      deleteButton.setAttribute("data-bs-target", "#staticBackdrop");
 
       ul.appendChild(liOne);
       ul.appendChild(liTwo);
       ul.appendChild(liThree);
       ul.appendChild(liFour);
       ul.appendChild(liFive);
+      ul.appendChild(liSix);
       ul.appendChild(updateButton);
       ul.appendChild(deleteButton);
 
@@ -124,7 +129,7 @@ function showLocalStorageData() {
   }
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   let formData;
 
@@ -155,6 +160,7 @@ form.addEventListener('submit', (event) => {
       companyName: companyName.value,
       address: address.value,
       additionalInfo: additionalInfo.value,
+      date: update ? creationDate : new Date().toLocaleString("en-GB"),
     };
     console.log(formData);
     if (update) {
@@ -169,19 +175,19 @@ form.addEventListener('submit', (event) => {
 });
 
 function resetForm() {
-  firstName.value = '';
-  lastName.value = '';
-  companyName.value = '';
-  address.value = '';
-  email.value = '';
-  phone.value = '';
-  additionalInfo.value = '';
+  firstName.value = "";
+  lastName.value = "";
+  companyName.value = "";
+  address.value = "";
+  email.value = "";
+  phone.value = "";
+  additionalInfo.value = "";
 
-  document.getElementById('submit').textContent = 'Save user';
+  document.getElementById("submit").textContent = "Save user";
 }
 
 function updateUser(id) {
-  let users = JSON.parse(localStorage.getItem('users'));
+  let users = JSON.parse(localStorage.getItem("users"));
   let userToUpdate = users.filter((user) => user.id === id)[0];
   updateId = userToUpdate.id;
   firstName.value = userToUpdate.firstName;
@@ -193,13 +199,14 @@ function updateUser(id) {
   additionalInfo.value = userToUpdate.additionalInfo;
 
   update = true;
-  document.getElementById('submit').textContent = 'Update User';
+  creationDate = userToUpdate.date;
+  document.getElementById("submit").textContent = "Update User";
 }
 
 function deleteUser(id) {
-  let users = JSON.parse(localStorage.getItem('users'));
+  let users = JSON.parse(localStorage.getItem("users"));
   let usersToSaveAgain = users.filter((user) => user.id !== id);
-  localStorage.setItem('users', JSON.stringify(usersToSaveAgain));
+  localStorage.setItem("users", JSON.stringify(usersToSaveAgain));
   showLocalStorageData();
 }
 
